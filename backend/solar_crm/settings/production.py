318fd7e5aa7387bbf3e15c6d77f222ccc23b3912
FawazMulla <139045793+FawazMulla@users.noreply.github.com>
2026-01-02 01:46:44 +0530

    change static file storage in prod

--- backend/solar_crm/settings/production.py
+++ backend/solar_crm/settings/production.py
@@ -23,7 +23,7 @@ DATABASES = {
 # Static Files Configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media Files
 MEDIA_URL = '/media/'
